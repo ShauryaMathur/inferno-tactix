808495c671575870b65446661589ae49b58960b0
Shaurya Mathur <47005444+ShauryaMathur@users.noreply.github.com>
2025-04-10 23:07:46 -0400

    Added sb3

---
+++ environment/train.py
@@ -0,0 +1,26 @@
+from wildfireenv import FireSimulationGymEnv
+from infernoenv import FireEnvSyncWrapper
+
+from stable_baselines3 import DQN
+from stable_baselines3.common.env_checker import check_env
+
+def main():
+    print("Starting training...")
+    # Create the async env
+    async_env = FireSimulationGymEnv()
+    print("Created async env",async_env)
+    # Wrap it
+    env = FireEnvSyncWrapper(async_env)
+    print("Wrapped env",env)
+    # Optional sanity check
+    check_env(env, warn=True)
+    print("Checked env")
+    # Train the agent
+    model = DQN("MlpPolicy", env, verbose=1)
+    model.learn(total_timesteps=10000)
+
+    # Save model
+    model.save("ppo_firefighter")
+
+if __name__ == "__main__":
+    main()
