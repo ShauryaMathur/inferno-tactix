5c621f569eeb3979f62394b93931849d7e8a8af6
Shaurya Mathur <47005444+ShauryaMathur@users.noreply.github.com>
2025-05-05 00:57:09 -0400

    Updated final report

--- tactix/src/models/simulation.ts
+++ tactix/src/models/simulation.ts
@@ -86,7 +86,7 @@ private connectSocket() {
   // const wsUrl = `ws://${host}:8765`;
   // this.socket = new WebSocket(wsUrl);
 
-  this.socket = new WebSocket("ws://python-backend:8765");
+  this.socket = new WebSocket("ws://localhost:8765");
   console.log("🌐 Connecting to WebSocket server...");
 
   this.socket.onopen = () => {
